75e81f89134f41baa35aa7fb7614c44622f745af
ViceVareles <vicente_vareles@hotmail.com>
2025-05-27 17:12:27 +0000

    voy por el 14

--- .learn/resets/013-sum_of_digits/app.py
+++ .learn/resets/013-sum_of_digits/app.py
@@ -1,9 +1,6 @@
 # Complete the function "digits_sum" so that it prints the sum of a three-digit number
 def digits_sum(num):
-  uno = int(str(num)[0])
-  dos = int(str(num)[1])
-  tres = int(str(num)[2])
-  return uno + dos + tres
+  return None
 
 
 # Invoke the function with any three-digit number
--- exercises/010-swap_digits/app.py
+++ exercises/010-swap_digits/app.py
@@ -1,9 +1,8 @@
 # Complete the function to return the swapped digits of a given two-digit integer
 def swap_digits(num):
   # Your code here
-  digito_uno = num // 10
-  digito_dos = num % 10
-  return digito_dos, digito_uno
+  aux = str(num)[1] + str(num)[0]
+  return int(aux)
    
 # Invoke the function with any two-digit integer as its argument
 print(swap_digits(30))
