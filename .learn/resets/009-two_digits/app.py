d36d4b8050bd652b992a43cfee092cbd6e0c6670
ViceVareles <vicente_vareles@hotmail.com>
2025-05-27 16:30:47 +0000

    ya voy por el 10

--- .learn/resets/009-two_digits/app.py
+++ .learn/resets/009-two_digits/app.py
@@ -1,9 +1,7 @@
 # Complete the function to return the tens digit and the units digit of any interger
 def two_digits(number):
   # Your code here
-  decenas = number // 10
-  diferencia = number % 10
-  return decenas,diferencia
+  return None
    
 
 # Invoke the function with any two digit integer as its argument
