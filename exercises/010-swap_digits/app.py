5c3d0f44edf113e10f6d6659f61b5d519e320b9f
ViceVareles <vicente_vareles@hotmail.com>
2025-05-27 16:35:58 +0000

    avanzando a paso firme

--- exercises/010-swap_digits/app.py
+++ exercises/010-swap_digits/app.py
@@ -1,7 +1,9 @@
 # Complete the function to return the swapped digits of a given two-digit integer
 def swap_digits(num):
   # Your code here
-  return None
+  digito_uno = num // 10
+  digito_dos = num % 10
+  return digito_dos, digito_uno
    
 # Invoke the function with any two-digit integer as its argument
 print(swap_digits(30))
